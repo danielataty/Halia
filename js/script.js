d2504a7e72f00a105351fc0a74cbc7527aef6593
Daniela Tati <141074542+danielatati@users.noreply.github.com>
2024-10-08 21:31:41 +0100

    New version
    Halia1.2

--- js/script.js
+++ js/script.js
@@ -10,14 +10,14 @@ hamburger.addEventListener('click', ()=> {
 const navbarEl = document.querySelector('.navbar');
 
 window.addEventListener("scroll", () => {
-    // Pega o valor atual de rolagem
+   
     const scrollY = window.scrollY;
 
-    // Se a rolagem for maior que a distância entre o início do bottomContainer e o topo da navbar
+  
     if (scrollY > 20) {
-        navbarEl.classList.add("active"); // Adiciona a classe "active"
+        navbarEl.classList.add("active"); 
     } else {
-        navbarEl.classList.remove("active"); // Remove a classe "active"
+        navbarEl.classList.remove("active"); 
     }
 });
 
@@ -28,7 +28,7 @@ const lunchBtn = document.getElementById('lunch-btn');
 const dinnerBtn = document.getElementById('dinner-btn');
 
 // Selecionando as seções de menu
-const introEl = document.getElementById('introduction'); // Verifique se o ID está correto (era 'introdution')
+const introEl = document.getElementById('introduction'); 
 const breakfastMenu = document.getElementById('breakfast');
 const lunchMenu = document.getElementById('lunch');
 const dinnerMenu = document.getElementById('dinner');
@@ -36,38 +36,38 @@ const dinnerMenu = document.getElementById('dinner');
 // Selecionando a imagem que vai mudar
 const menuImg = document.getElementById('menu-img');
 
-// Função para mostrar o menu correto e mudar a imagem
+// Função para mostra o menu correto e mudar a imagem
 function showMenu(menuToShow, btnToActivate, imgSrc) {
-  // Esconder todos os menus
+  // Esconde todos os menus
   breakfastMenu.style.display = 'none';
   lunchMenu.style.display = 'none';
   dinnerMenu.style.display = 'none';
-  introEl.style.display = 'none'; // Esconder introdução quando algum menu é exibido
+  introEl.style.display = 'none'; 
 
-  // Remover a classe 'active' de todos os botões
+  // Remove a classe 'active' de todos os botões
   breakfastBtn.classList.remove('active');
   lunchBtn.classList.remove('active');
   dinnerBtn.classList.remove('active');
 
-  // Mostrar o menu selecionado
+  // Mostra o menu selecionado
   menuToShow.style.display = 'block';
 
-  // Adicionar a classe 'active' ao botão clicado
+  // Adiciona a classe 'active' ao botão clicado
   btnToActivate.classList.add('active');
 
-  // Alterar a imagem
+  // Altera a imagem
   menuImg.src = imgSrc;
 }
 
 // Adicionar event listeners aos botões
 breakfastBtn.addEventListener('click', function() {
-  showMenu(breakfastMenu, breakfastBtn, 'img/bf2.jpg'); // Caminho para a imagem do Breakfast
+  showMenu(breakfastMenu, breakfastBtn, 'img/bf1.jpg'); 
 });
 
 lunchBtn.addEventListener('click', function() {
-  showMenu(lunchMenu, lunchBtn, 'img/lunch1.jpg'); // Caminho para a imagem do Lunch
+  showMenu(lunchMenu, lunchBtn, 'img/lunch2.jpg');
 });
 
 dinnerBtn.addEventListener('click', function() {
-  showMenu(dinnerMenu, dinnerBtn, 'img/dinner2.jpg'); // Caminho para a imagem do Dinner
+  showMenu(dinnerMenu, dinnerBtn, 'img/app3.jpg'); 
 });
