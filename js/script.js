aab72819244a7a31f89b507ea52fc393b0e7e0bb
Daniela Tati <141074542+danielatati@users.noreply.github.com>
2024-10-14 23:57:57 +0100

    atualizações

--- js/script.js
+++ js/script.js
@@ -1,28 +1,26 @@
 const hamburger = document.querySelector('.hamburger')
-
 const nav = document.querySelector('.navlinks')
 
-
-hamburger.addEventListener('click', ()=> {
-    nav.classList.toggle("active")
+hamburger.addEventListener("click", ()=> {
+  nav.classList.toggle("active")
 })
 
 const navbarEl = document.querySelector('.navbar');
 
 window.addEventListener("scroll", () => {
    
-    const scrollY = window.scrollY;
+  const scrollY = window.scrollY;
 
-  
-    if (scrollY > 20) {
-        navbarEl.classList.add("active"); 
-    } else {
-        navbarEl.classList.remove("active"); 
-    }
+  if (scrollY > 20) {
+    navbarEl.classList.add("active"); 
+  } 
+  else {
+    navbarEl.classList.remove("active"); 
+  }
+
 });
 
-
-// Selecionando os botões
+//Menu - section Selecionando os botões
 const breakfastBtn = document.getElementById('breakfast-btn');
 const lunchBtn = document.getElementById('lunch-btn');
 const dinnerBtn = document.getElementById('dinner-btn');
@@ -36,26 +34,26 @@ const dinnerMenu = document.getElementById('dinner');
 // Selecionando a imagem que vai mudar
 const menuImg = document.getElementById('menu-img');
 
-// Função para mostra o menu correto e mudar a imagem
+// Função para mostrar o menu correto e mudar a imagem
 function showMenu(menuToShow, btnToActivate, imgSrc) {
-  // Esconde todos os menus
+  // Esconder todos os menus
   breakfastMenu.style.display = 'none';
   lunchMenu.style.display = 'none';
   dinnerMenu.style.display = 'none';
   introEl.style.display = 'none'; 
 
-  // Remove a classe 'active' de todos os botões
+  // Remover a classe 'active' de todos os botões
   breakfastBtn.classList.remove('active');
   lunchBtn.classList.remove('active');
   dinnerBtn.classList.remove('active');
 
-  // Mostra o menu selecionado
+  // Mostrar o menu selecionado
   menuToShow.style.display = 'block';
 
-  // Adiciona a classe 'active' ao botão clicado
+  // Adicionar a classe 'active' ao botão clicado
   btnToActivate.classList.add('active');
 
-  // Altera a imagem
+  // Alterar a imagem
   menuImg.src = imgSrc;
 }
 
